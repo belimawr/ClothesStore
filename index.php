87a6bf995f31b32e52f948f6c82b16e06f564552
Tiago Queiroz <contato@tiago.eti.br>
2013-04-16 12:12:34 +0100

    first commig

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem('./templates');
+$twig = new Twig_Environment($loader);
+
+$template = $twig->loadTemplate('index2.phtml');
+
+/*
+ * Database data
+ */
+$host = "s.tiago.eti.br";
+$database = "StrathWEB_Store";
+$user = "StrathWEB";
+$pass = "2013StrathWEB";
+
+/*
+ * Connect to database.
+ */
+
+mysql_connect($host, $user, $pass);
+mysql_select_db($database) or die(mysql_error());
+
+/* Query the messages to the index page */
+$messages = mysql_query("SELECT * FROM index_data");
+$parameters = array();
+while ($row = mysql_fetch_array($messages))
+{
+	$parameters[$row['msg_name']] = $row['msg'];
+	$parameters[$row['img_name']] = $row['img'];
+}
+
+/* Query the images to the index slideShow */
+$messages = mysql_query("SELECT img, img_name FROM index_data WHERE img_name like 'slideShow'");
+$images = array();
+$i = 0;
+while ($row = mysql_fetch_array($messages))
+{
+	$images[$i++] = $row['img'];
+}
+$parameters['slideShowImages'] = $images;
+$parameters['imgCount'] = $i;
+
+//Using others variables to store data in a array
+//It will be useful to read from the database and add
+//the data to the array
+// $vecKey = 'img4';
+// $vecEl = "source images/Womens Jeans/wj3.jpg";
+
+// $parameters[$vecKey] = $vecEl;
+// print_r($parameters);
+$template->display($parameters);
+?>
---
+++ womens.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem('./templates');
+$twig = new Twig_Environment($loader);
+
+$template = $twig->loadTemplate('Womenjacket.phtml');
+
+/*
+ * Database data
+ */
+$host = "s.tiago.eti.br";
+$database = "StrathWEB_Store";
+$user = "StrathWEB";
+$pass = "2013StrathWEB";
+
+/*
+ * Connect to database.
+ */
+
+mysql_connect($host, $user, $pass);
+mysql_select_db($database) or die(mysql_error());
+
+/* Query the messages to the index page */
+$depart = $_GET['depart'];
+$type = $_GET['type'];
+$cloths = mysql_query("SELECT style_ID, description, department, type, thumbnail_link FROM style WHERE department like '$depart' and type like '$type'") or die(mysql_error());
+$parameters = array();
+$wcloths = array();
+while ($row = mysql_fetch_array($cloths))
+{
+	$tmp = array();
+	$tmp['id'] = $row['style_ID'];
+	$tmp['desc'] = $row['description'];
+	$tmp['src'] = $row['thumbnail_link'];
+	$tmp['price'] = 100;
+	$wcloths[] = $tmp;
+}
+$parameters['womensCloths'] = $wcloths;
+// $parameters['department'] = "Coats & Jackets";
+$parameters['department'] = $_GET['department'];
+
+
+$template->display($parameters);
+?>
