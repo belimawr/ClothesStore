139b72ceec883e4713afbece8100e8d2bb8766ea
Tiago Queiroz <contato@tiago.eti.br>
2013-04-17 23:03:49 +0100

    secure way to store passwords

--- addUser.php
+++ addUser.php
@@ -19,6 +19,8 @@ $phone = $db->escape_string($_POST['phone']);
 $mobile = $db->escape_string($_POST['mobile']);
 $email = $db->escape_string($_POST['email']);
 
+$password = hashPassword($password, $email);
+
 if($db->connect_errno > 0)
 {
 	die('Unable to connect to the database $database at $host, error: [' . $db->connect_error . ']');
@@ -37,5 +39,54 @@ if(!$result = $db->query($myquery))
 }
 
 echo("Your username is: " . $email . " and your password is: " . $password);
+
+// echo("<BR><BR>");
+
+// echo(hashPassword($password, $email));
+
+// echo("<BR<BR>");
+
+// echo(validadeHashedPassword($password, $email, hashPassword($password, $email)));
 $db->close();
+
+function hashPassword($pass, $user)
+{
+	/* Generetes a random and secure 256 bit (64 characteres) */
+	$salt = hash('sha256', uniqid(mt_rand(), true) . 'one random part here' . strtolower($user));
+	
+	/* prefix the hash with the salt */
+	$hash = $salt . $pass;
+	
+	/* Hash the password a bunch of times */
+	for($i = 0; $i < 10000; $i++)
+	{
+		$hash = hash('sha256', $hash);
+	}
+	
+	/* Put the salt at the beggining, so I can find it later */
+	$hash = $salt . $hash;
+	
+	return $hash;
+}
+
+function validadeHashedPassword($pass, $user, $hashed)
+{
+	/* Get the salt */
+	$salt = substr($hashed, 0, 64);
+	
+	/* prefix the hash with the salt */
+	$hash = $salt . $pass;
+	
+	/* Hash the password a bunch of times */
+	for($i = 0; $i < 10000; $i++)
+	{
+	$hash = hash('sha256', $hash);
+	}
+	
+	/* put the salt at the beggining again */
+	$hash = $salt . $hash;
+	
+	/* Return if it's equal or not to the stored password */
+	return $hash == $hashed;
+}
 ?>
