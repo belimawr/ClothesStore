4c970f5725ba737925941a3b7e6b80a9bf6244e8
Tiago Queiroz <contato@tiago.eti.br>
2013-04-17 13:12:51 +0100

    non deprecated functions

--- addUser.php
+++ addUser.php
@@ -6,32 +6,36 @@ $user = "StrathWEB";
 $pass = "2013StrathWEB";
 $table = "customer";
 
-$password = $_POST['password'];
-$Fname = $_POST['Fname'];
-$Lanme = $_POST['Lname'];
-$address1 = $_POST['address1'];
-$address2 = $_POST['address2'];
-$city = $_POST['city'];
-$postcode = $_POST['postcode'];
-$phone = $_POST['phone'];
-$mobile = $_POST['mobile'];
-$email = $_POST['email'];
+$db = new mysqli($host, $user, $pass, $database);
 
+$password = $db->escape_string($_POST['password']);
+$Fname = $db->escape_string($_POST['Fname']);
+$Lanme = $db->escape_string($_POST['Lname']);
+$address1 = $db->escape_string($_POST['address1']);
+$address2 = $db->escape_string($_POST['address2']);
+$city = $db->escape_string($_POST['city']);
+$postcode = $db->escape_string($_POST['postcode']);
+$phone = $db->escape_string($_POST['phone']);
+$mobile = $db->escape_string($_POST['mobile']);
+$email = $db->escape_string($_POST['email']);
 
-mysql_connect($host, $user, $pass);
-mysql_select_db($database) or die(mysql_error());
+if($db->connect_errno > 0)
+{
+	die('Unable to connect to the database $database at $host, error: [' . $db->connect_error . ']');
+}
 
-// $db = new mysqli($host, $user, $pass, $database);
-
-// if($db->connect_errno > 0
-// {
-// 	die('Unable to connect to database [' . $db->connect_error . ']');
-// }
-
-mysql_query(
-		"insert into $table (customer_ID, password, name_f, name_l, address_1, address_2, city, postcode, phone, mobile, email)
-		values(null, '$password', '$Fname' , '$Lanme', '$address1', '$address2', '$city', '$postcode', '$phone', '$mobile', '$email')") or die(mysql_error());
+$myquery = <<<SQL
+		 INSERT INTO
+		 $table (customer_ID, password, name_f, name_l, address_1, address_2, city, postcode, phone, mobile, email)
+		 VALUES
+		 (null, '$password', '$Fname' , '$Lanme', '$address1', '$address2', '$city', '$postcode', '$phone', '$mobile', '$email')
+SQL;
+$result = null;
+if(!$result = $db->query($myquery))
+{
+	die('There was an error running the query [' . $db->error . ']');
+}
 
 echo("Your username is: " . $email . " and your password is: " . $password);
-
+$db->close();
 ?>
