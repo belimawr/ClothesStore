be36ef631edf3f55e13964a41fd3026f74501c04
Tiago Queiroz <contato@tiago.eti.br>
2013-04-17 12:15:40 +0100

    add user form working

---
+++ addUser.php
@@ -0,0 +1,37 @@
+<?php 
+
+$host = "s.tiago.eti.br";
+$database = "StrathWEB_Store";
+$user = "StrathWEB";
+$pass = "2013StrathWEB";
+$table = "customer";
+
+$password = $_POST['password'];
+$Fname = $_POST['Fname'];
+$Lanme = $_POST['Lname'];
+$address1 = $_POST['address1'];
+$address2 = $_POST['address2'];
+$city = $_POST['city'];
+$postcode = $_POST['postcode'];
+$phone = $_POST['phone'];
+$mobile = $_POST['mobile'];
+$email = $_POST['email'];
+
+
+mysql_connect($host, $user, $pass);
+mysql_select_db($database) or die(mysql_error());
+
+// $db = new mysqli($host, $user, $pass, $database);
+
+// if($db->connect_errno > 0
+// {
+// 	die('Unable to connect to database [' . $db->connect_error . ']');
+// }
+
+mysql_query(
+		"insert into $table (customer_ID, password, name_f, name_l, address_1, address_2, city, postcode, phone, mobile, email)
+		values(null, '$password', '$Fname' , '$Lanme', '$address1', '$address2', '$city', '$postcode', '$phone', '$mobile', '$email')") or die(mysql_error());
+
+echo("Your username is: " . $email . " and your password is: " . $password);
+
+?>
---
+++ checkusername.php
@@ -0,0 +1,21 @@
+<?php 
+
+$host = "s.tiago.eti.br";
+$database = "StrathWEB_Store";
+$user = "StrathWEB";
+$pass = "2013StrathWEB";
+$table = "customer";
+
+$email = $_POST['email'];
+
+mysql_connect($host, $user, $pass);
+mysql_select_db($database) or die(mysql_error());
+
+$query = mysql_query("SELECT email FROM $table WHERE email like '$email'") or die(mysql_error());
+
+if(mysql_num_rows($query) == 0)
+	echo("ok");
+else
+	echo("error");
+
+?>
