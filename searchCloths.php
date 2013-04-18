a0931de1c371c83454ff787fd774bba5ab536962
Tiago Queiroz <contato@tiago.eti.br>
2013-04-18 11:43:48 +0100

    hange log: - womens.php renamed to search_Menu.php - templates/Womenjacket.phtml renamed to templates/search_Menu.phtml - testchk.php renamed to searchCloths.php - Search form working. It searches using the department and type chose in the menu and the checkboxes.

---
+++ searchCloths.php
@@ -0,0 +1,119 @@
+<?php 
+
+require_once dirname(__FILE__).'/../lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem('./templates');
+$twig = new Twig_Environment($loader);
+
+$template = $twig->loadTemplate('searchCloths.phtml');
+
+$parameters = array();
+
+$size_list = "";
+if(isset($_POST['size_list']))
+{
+	foreach($_POST['size_list'] as $check)
+	{
+		$parameters[$check] = 1;
+		if(strlen($size_list) == 0)
+		{
+			$size_list = "(item_size like '$check') ";
+		}
+		else
+		{
+			$size_list = $size_list . " or (item_size like '$check') ";
+		}
+	}
+}
+$colour_list = "";
+if(isset($_POST['colour_list']))
+{
+	foreach($_POST['colour_list'] as $check)
+	{
+		$parameters[$check] = 1;
+		if(strlen($colour_list) == 0)
+		{
+			$colour_list = "(colour like '$check') ";
+		}
+		else
+		{
+			$colour_list = $colour_list . " or (colour like '$check') ";
+		}
+	}
+}
+
+$dep_query = $_POST['dep_query'];
+$type_query = $_POST['type_query'];
+
+/*
+ * Database data
+ */
+$host = "s.tiago.eti.br";
+$database = "StrathWEB_Store";
+$user = "StrathWEB";
+$pass = "2013StrathWEB";
+
+/*
+ * Connect to database.
+ */
+$db = new mysqli($host, $user, $pass, $database);
+
+/* Create the query */
+if($dep_query == "all")
+	$string_Dept = " true";
+else
+{
+	if($type_query != "any")
+		$string_Dept = "((department like '$dep_query') and (type like '$type_query'))";
+	else
+		$string_Dept = "(department like '$dep_query')";
+}
+
+if( (strlen($colour_list) > 0) && (strlen($size_list) > 0))
+{
+	$query = "SELECT * FROM item JOIN style on item.style_ID = style.style_ID WHERE ($colour_list or $size_list) and $string_Dept";
+}
+elseif(strlen($colour_list) > 0)
+{
+	$query = "SELECT * FROM item JOIN style on item.style_ID = style.style_ID WHERE ($colour_list) and $string_Dept";
+}
+elseif(strlen($size_list) > 0)
+{
+	$query = "SELECT * FROM item JOIN style on item.style_ID = style.style_ID WHERE ($size_list) and $string_Dept";
+}
+
+/* Execute the query */
+if(isset($query))
+{
+	$result = $db->query($query);
+
+	/* item_ID | style_ID | colour | item_size | price | stock | image_link | style_ID | description  | name  | department | type  | material | thumbnail_link */
+	while ($row = $result->fetch_assoc())
+	{
+		$tmp = array();
+		$tmp['id'] = $row['style_ID'];
+		$tmp['desc'] = $row['description'];
+		$tmp['src'] = $row['image_link'];
+		$tmp['price'] = $row['price'];
+		$tmp['name'] = $row['name'];
+		$tmp['size'] = $row['item_size'];
+		$tmp['material'] = $row['material'];
+		$tmp['thumbnail'] = $row['thumbnail_link'];
+		$tmp['colour'] = $row['colour'];
+		$wcloths[] = $tmp;
+	}
+}
+if(isset($wcloths))
+	$parameters['womensCloths'] = $wcloths;
+
+$parameters['department'] = $_POST['department'];
+$parameters['dep_query'] = $dep_query;
+$parameters['type_query'] = $type_query;
+
+$db->close();
+
+$template->display($parameters);
+
+
+?>
---
+++ search_Menu.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem('./templates');
+$twig = new Twig_Environment($loader);
+
+$template = $twig->loadTemplate('search_Menu.phtml');
+
+/*
+ * Database data
+ */
+$host = "s.tiago.eti.br";
+$database = "StrathWEB_Store";
+$user = "StrathWEB";
+$pass = "2013StrathWEB";
+
+/*
+ * Connect to database.
+ */
+$db = new mysqli($host, $user, $pass, $database);
+
+/* Query the messages to the index page */
+$depart = $_GET['depart'];
+$type = $_GET['type'];
+
+if($depart == "all")
+	$query = "SELECT style_ID, description, department, type, thumbnail_link FROM style";
+else
+{
+	if($type != "any")
+		$query = "SELECT style_ID, description, department, type, thumbnail_link FROM style WHERE department like '$depart' and type like '$type'";
+	else
+		$query = "SELECT style_ID, description, department, type, thumbnail_link FROM style WHERE department like '$depart'";
+}
+
+$result = $db->query($query);
+
+$parameters = array();
+$wcloths = array();
+while ($row = $result->fetch_assoc())
+{
+	$tmp = array();
+	$tmp['id'] = $row['style_ID'];
+	$tmp['desc'] = $row['description'];
+	$tmp['src'] = $row['thumbnail_link'];
+	$tmp['price'] = 100;
+	$wcloths[] = $tmp;
+}
+$parameters['womensCloths'] = $wcloths;
+$parameters['dep_query'] = $depart;
+$parameters['type_query'] = $type;
+$parameters['department'] = $_GET['department'];
+
+$db->close();
+
+$template->display($parameters);
+?>
