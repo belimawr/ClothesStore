146a40b59e369a372bd98133812234423b276deb
Tiago Queiroz <contato@tiago.eti.br>
2013-04-19 17:19:38 +0100

    validating e-mail with php function

--- checkusername.php
+++ checkusername.php
@@ -10,14 +10,21 @@ $email = $_POST['email'];
 
 $db = new mysqli($host, $user, $pass, $database);
 
-$query = "SELECT email FROM $table WHERE email like '$email'";
+$query = "SELECT email FROM $table WHERE email like '" . $db->real_escape_string($email) . "'";
 
 $result = $db->query($query);
 
-if($result->num_rows == 0)
-	echo("ok");
+if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+{
+	echo("emailError");
+}
 else
-	echo("error");
+{
+	if($result->num_rows == 0)
+		echo("ok");
+	else
+		echo("error");
+}
 
 $db->close();
 
