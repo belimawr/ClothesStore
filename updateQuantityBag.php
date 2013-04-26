18e9a77dbaf9bb340924406bace6dda4152a5b19
Tiago Queiroz <contato@tiago.eti.br>
2013-04-26 15:12:38 +0100

    updating quantity in bag

---
+++ updateQuantityBag.php
@@ -0,0 +1,36 @@
+<?php
+/* Checks if exists a session */
+session_start();
+if(isset($_SESSION['uid']))
+{
+	$parameters['logged'] = 1;
+	$parameters['username'] = $_SESSION['username'];
+	$customer_ID = $_SESSION['customer_ID'];
+}
+else
+{
+	$parameters['logged'] = 0;
+	session_unset();
+	session_destroy();
+	session_write_close();
+	setcookie(session_name(),'',0,'/');
+	$_SESSION = array();
+}
+
+$host = "s.tiago.eti.br";
+$database = "StrathWEB_Store";
+$user = "StrathWEB";
+$pass = "2013StrathWEB";
+$table = "customer";
+
+$db = new mysqli($host, $user, $pass, $database);
+
+$itemid = $db->escape_string($_POST['itemID']);
+$quantity = $db->escape_string($_POST['quantity']);
+
+$result = $db->query("UPDATE basket
+					  SET quantity=$quantity
+					  WHERE item_ID = $itemid 
+					  AND customer_ID= $customer_ID");
+echo($db->error);
+?>
